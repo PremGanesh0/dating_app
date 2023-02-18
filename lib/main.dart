9cb1325c384b3f480adc9203d3b1b3efac307784
PremGanesh <premganesh2655@gmail.com>
2023-02-18 10:18:09 +0530

    hello world comment

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
 void main() async {
-
+// hellooo world
   // Initialized before calling runApp to init firebase app
   WidgetsFlutterBinding.ensureInitialized();
 
